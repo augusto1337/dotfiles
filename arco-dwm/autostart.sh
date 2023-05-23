f28265ddd8a7d19c7871ca8db6499dce8c134a85
spectrum007 <kodopav134@fanneat.com>
2023-05-22 21:38:42 -0300

    my dotfiles

---
+++ arco-dwm/autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#function run {
+ #if ! pgrep $1 ;
+  #then
+   # $@&
+  #fi
+#}
+
+#setxkbmap -model abnt2 -layout br -variant abnt2
+#setxkbmap -model abnt2 -layout br -variant abnt2
+#setxkbmap -model abnt2 -layout br -variant abnt2
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+#run "numlockx on"
+#run "volumeicon"
+slstatus &
+sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
+#run "nitrogen --restore"
+#xfce4-power-manager
+#run "conky -c $HOME/.config/arco-dwm/system-overview"
+#you can set wallpapers in themes as well
+#feh --bg-fill --no-fehbg $HOME/Downloads/arcolinux/background29.jpg
+#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
